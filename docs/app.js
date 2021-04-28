88dc07cca54b53102c04aa6b900d3820c455ee9b
Suleeyman <ozturkyavuz2412@gmail.com>
2021-04-28 19:47:55 +0200

    responsive mobile design !

--- docs/app.js
+++ docs/app.js
@@ -1,6 +1,19 @@
 const toggle = document.querySelector('.toggle')
+const overlay = document.querySelector('.overlay')
 
+let countToggle = 0;
 toggle.addEventListener('click', () => {
-    console.log(toggle.nextElementSibling);
+    countToggle++;
     toggle.nextElementSibling.classList.toggle('is-open')
+    if ((countToggle % 2) === 1) {
+        toggle.src = "./images/icon-close-menu.svg"
+        document.body.style.overflowY = "hidden"
+        overlay.classList.add('active')
+        toggle.closest('nav').style.zIndex = "2"
+    } else {
+        toggle.src = "./images/icon-hamburger.svg"
+        document.body.style.overflowY = "auto"
+        overlay.classList.remove('active')
+        toggle.closest('nav').style.zIndex = "0"
+    }
 })

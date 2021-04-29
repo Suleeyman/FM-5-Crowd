b3e303b245a250597686d585def41bb8e101cf91
Suleeyman <ozturkyavuz2412@gmail.com>
2021-04-29 12:19:44 +0200

    Close navbar

--- docs/app.js
+++ docs/app.js
@@ -1,8 +1,28 @@
 const toggle = document.querySelector('.toggle')
 const overlay = document.querySelector('.overlay')
+const selectCards = document.querySelectorAll('.select-content > .bg-cyan:not(.disabled)')
+
+toggle.addEventListener('click', toggleNavbar)
+overlay.addEventListener('click', toggleNavbar)
+
+selectCards.forEach(btn => {
+    btn.addEventListener('click', function(e) {
+        e.preventDefault()
+        console.log(btn.dataset);
+    })
+})
+
+document.addEventListener('keyup', (e) => {
+    console.log(e.key);
+    if(e.key === 'Escape' && (countToggle % 2) === 1) {
+        toggleNavbar()
+    }
+})
+
 
 let countToggle = 0;
-toggle.addEventListener('click', () => {
+
+function toggleNavbar() {
     countToggle++;
     toggle.nextElementSibling.classList.toggle('is-open')
     if ((countToggle % 2) === 1) {
@@ -16,4 +36,4 @@ toggle.addEventListener('click', () => {
         overlay.classList.remove('active')
         toggle.closest('nav').style.zIndex = "0"
     }
-})
+}
